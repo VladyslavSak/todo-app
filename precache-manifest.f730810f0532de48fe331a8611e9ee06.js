47d346d25539e00d5be9191076dc3bf2d71cda74
VladyslavSak <iblackvour@gmail.com>
2019-10-10 20:36:46 +0300

    Updates

---
+++ precache-manifest.f730810f0532de48fe331a8611e9ee06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a0af930159475c510a27aafed4b603",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "56ba49dc05df645dc004",
+    "url": "/react_todo-app/static/css/main.5130741e.chunk.css"
+  },
+  {
+    "revision": "c25912838c72be9128d3",
+    "url": "/react_todo-app/static/js/2.81f5c314.chunk.js"
+  },
+  {
+    "revision": "56ba49dc05df645dc004",
+    "url": "/react_todo-app/static/js/main.f9af27b5.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
